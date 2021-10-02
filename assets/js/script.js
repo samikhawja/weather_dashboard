e062e61a9336f389fcaea2c9d777daacd8869274
samikhawja <skhawja11@gmail.com>
2021-10-02 12:16:06 -0700

    framewire complete

---
+++ assets/js/script.js
@@ -0,0 +1,26 @@
+var today = moment();
+
+
+function myFunction() {
+  window.history.back();
+}
+function myFunction() {
+    window.history.go(-2);
+}
+  function myFunction() {
+    window.history.go(-3);
+}
+  function myFunction() {
+    window.history.go(-4);
+}
+  function myFunction() {
+    window.history.go(-5);
+}
+  function myFunction() {
+    window.history.go(-6);
+}
+  function myFunction() {
+    window.history.go(-7);
+}
+
+$("#date").text(today.format("MMM Do, YYYY"));
