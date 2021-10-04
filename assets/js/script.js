787ff286e5ff598d9f7c3bf0efd5dd17d3638161
samikhawja <skhawja11@gmail.com>
2021-10-04 16:18:48 -0700

    added presentcity weather

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,72 +1,52 @@
-var searchInputEl = document.querySelector('#search');
-var maincardContainerEl = document.querySelector('#maincard');
-var searchTerm = document.querySelector('#date');
+var city = ""
 var today = moment();
+var APIKey="01ba57fd295b828746a188dea79c4db5"
 
-$("#date").text(today.format("MMM Do, YYYY"));
 
 
-function myFunction() {
-  window.history.back();
-}
-function myFunction() {
-    window.history.go(-2);
-}
-  function myFunction() {
-    window.history.go(-3);
-}
-  function myFunction() {
-    window.history.go(-4);
-}
-  function myFunction() {
-    window.history.go(-5);
-}
-  function myFunction() {
-    window.history.go(-6);
-}
-  function myFunction() {
-    window.history.go(-7);
-}
 
-var formSubmitHandler = function (event) {
-    event.preventDefault();
-  
-    var location = searchInputEl.value.trim();
-  
-    if (location) {
-      geocodingAPI(location);
-  
-      maincardContainerEl.textContent = '';
-      searchInputEl.value = '';
-    } else {
-      alert('Please enter a location');
-    }
-    console.log(location)
-};
-  
-var buttonClickHandler = function (event) {
-    var language = event.target.getAttribute('data-language');
-  
-    if (language) {
-      getFeaturedRepos(language);
-  
-      maincardContainerEl.textContent = '';
-    }
-};
-
-var geocodingAPI = function() {
-    var apiUrl = 'http://api.openweathermap.org/geo/1.0/direct?q=' + location + '&limit=5&appid=01ba57fd295b828746a188dea79c4db5'
-    fetch(apiUrl)
-      .then(function (response) {
-        if (response.ok) {
-          response.json().then(function (data) {
-            displayRepos(data, user);
-          });
-        } else {
-          alert('Error: ' + response.statusText);
+function presentCity(city){
+    city = $("#city_search").val()
+    var weatherURL = "https://api.openweathermap.org/data/2.5/weather?q=" + city + "&APPID=" + APIKey;
+    $.ajax({
+        url : weatherURL,
+        method : "GET",
+    }).then(function(response){
+        console.log(response);
+        var cityN = city.toUpperCase()
+        $(".city-name").text(" " + cityN + " (" + (today.format("MMM Do, YYYY")) + ") "+ "icon")
+        var temp = Math.round((response.main.temp - 273.15) * 1.8 + 32);
+        $("#temperature").text(" " + temp + "°F")
+        var humd = (response.main.humidity);
+        $("#humidity").text(" " + humd + "%")
+        var wind = (response.wind.speed);
+        $("#wind_speed").text(" " + wind + "mph")
+        var lon = (response.coord.lon)
+        var lat = (response.coord.lat)
+        var UVIAPI = "https://api.openweathermap.org/data/2.5/onecall?lat=" + lat + "&lon=" + lon + "&appid=" + APIKey;
+        $.ajax ({
+            url : UVIAPI,
+            method : "GET",
+        }).then(function(response){
+            console.log(response)
+            var UVIn = (response.current.uvi);
+        $("#uvi").text(" " + UVIn)
+        if (UVIn <= 4) {
+            $("#uvi").css("background-color", "green")
         }
-      })
-      .catch(function (error) {
-        alert('Unable to connect to GitHub');
-      });
-  };
+        else if (UVIn >= 5 && UVIn <= 7) {
+            $("#uvi").css("background-color", "yellow")
+        }
+        else {
+            $("#uvi").css("background-color", "red")
+        }
+        })
+    })
+console.log()
+
+
+
+
+}
+
+$("#sbutton").on("click", presentCity)
